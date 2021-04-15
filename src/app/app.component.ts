4df92042e19f446402f3ef4ae2673034aea0d092
aldelgad <61358359+aldelgad@users.noreply.github.com>
2021-04-15 23:52:39 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'minibio';
+  socialNetworks = [
+    {
+      img: '../assets/images/github.png',
+      text: 'Github',
+      link: 'https://github.com/aldelgad'
+    },
+    {
+      img: '../assets/images/twitter.png',
+      text: 'Twitter',
+      link: 'https://twitter.com/penks23'
+    },
+    {
+      img: '../assets/images/instagram.png',
+      text: 'Instagram',
+      link: 'https://www.instagram.com/varo50'
+    },
+    {
+      img: '../assets/images/discord.png',
+      text: 'Discord',
+      link: 'https://discord.gg/vf9cM7stBG'
+    }
+  ]
+
+  imagesGallery = [
+    {
+      img: '../assets/images/draw.jpg'
+    },
+    {
+      img: '../assets/images/fantasy.jpg'
+    },
+    {
+      img: '../assets/images/space.jpg'
+    },
+    {
+      img: '../assets/images/warrior.jpg'
+    }
+  ]
+}
